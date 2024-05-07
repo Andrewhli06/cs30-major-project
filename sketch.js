c15fd20f92a39511908ec62a2a617f6d304deb74
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-05-07 14:09:03 -0600

    Basic bullet testing, need to determine grid size for level in order to delete bullets, and need to control direction

--- sketch.js
+++ sketch.js
@@ -1,17 +1,70 @@
-// Project Title
-// Your Name
-// Date
+// Awesome Tanks Game
+// Andrew Li
+// June 15th 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+class Bullet {
+  constructor(x, y, dx, dy) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = 5;
+  }
+  display() {
+    fill("black");
+    circle(this.x, this.y, this.radius);
+  }
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+}
 
+// class Player {
+//   constructor() {
+
+//   }
+//   display() {
+
+//   }
+//   move() {
+
+//   }
+// }
+
+// class EnemyTank {
+//   constructor() {
+
+//   }
+//   display() {
+
+//   }
+//   move() {
+
+//   }
+// }
+
+let bullets = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
   background(220);
+  for (let bullet of bullets) {
+    bullet.move();
+    bullet.display();
+  } 
   
-  square(mouseX, mouseY, 100);
 }
+
+function mousePressed() {
+  bullets.push(new Bullet(mouseX, mouseY, random(-5,5), random(-5,5)));
+  console.log(bullets);
+}
+
+
