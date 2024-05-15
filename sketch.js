4e901be85a2b7b999928f8f6a81db35d5d82be5f
Andrewhli06 <161518219+Andrewhli06@users.noreply.github.com>
2024-05-15 14:10:06 -0600

    Successful character and bullet movement, bullet deletion function, and basic 2D grid function.

--- sketch.js
+++ sketch.js
@@ -8,28 +8,25 @@
 class Bullet {
   constructor(x, y, tx, ty, dx, dy) {
     this.pos = createVector(x, y)
-    this.dx = dx
+    this.dx = dx;
     this.dy = dy;
-    this.transX = tx;
-    this.transY = ty;
+    this.tx = tx;
+    this.ty = ty;
     this.radius = 5;
-    this.rotX = mouseX - this.transX;
-    this.rotY = mouseY - this.transY;
-    this.rotateAngle = atan2(this.rotY, this.rotX) - 90;
+    this.rotX = mouseX - this.tx;
+    this.rotY = mouseY - this.ty;
+    this.rotateAngle = atan2(this.rotY , this.rotX) - 90;
   }
   display() {
     push();
-    translate(this.transX, this.transY);
+    translate(this.tx, this.ty);
     rotate(this.rotateAngle);
     fill("black");
     circle(this.pos.x, this.pos.y, this.radius);
     pop();
   }
-  update() {
+  move() {
     this.pos.y += this.dy;
-    this.rotX = mouseX - this.transX;
-    this.rotY = mouseY - this.transY;
-    this.rotateAngle = atan2(this.rotY, this.rotX) - 90;
   }
 }
 
@@ -71,32 +68,91 @@ class Player {
     this.rotateY = mouseY - this.transY;
     this.rotateAngle = atan2(this.rotateY, this.rotateX)
   }
+  update() {
+    character.move();
+    character.rotate();
+    character.display();
+  }
 }
 
+let tx, ty;
+let dx, dy;
+let bulletTravelDistance;
+let gridSize;
 let bullets = [];
 let character;
-let tx, ty;
+const CELLSIZE = 50;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER)
   angleMode(DEGREES);
+  dx = 5;
+  dy = 5;
   tx = width/2;
-  ty = height/2
-  character = new Player(0, 0, 5, 5, tx, ty, 50);
+  ty = height/2;
+  bulletTravelDistance = 200;
+  gridSize = 10;
+  character = new Player(0, 0, dx, dy, tx, ty, 50);
+  level1 = grid(gridSize, gridSize);
 }
 
 function draw() {
   background(200);
-  character.rotate();
-  character.move();
-  character.display();
+  character.update();
+  movement();
+  bulletDelete();
   for (let bullet of bullets) {
-    bullet.update();
+    bullet.move();
     bullet.display();
   } 
-  console.log(bullets);
+  //displayGrid(level1);
+}
+
+function movement() {
+  if (keyIsDown(87)) {
+    ty -= dy;
+  }
+  else if (keyIsDown(83)) {
+    ty += dy;
+  }
+  else if (keyIsDown(65)) {
+    tx -= dx;
+  }
+  else if (keyIsDown(68)) {
+    tx += dx;
+  }
 }
 
 function mousePressed() {
   bullets.push(new Bullet(0, 0, tx, ty, 5, 5));
+}
+
+function bulletDelete() {
+  for (let someBullet of bullets) {
+    if (dist(someBullet.pos.x, someBullet.pos.y, character.position.x, character.position.y) > bulletTravelDistance) {
+      let theIndex = bullets.indexOf(someBullet);
+      bullets.splice(theIndex, 1);
+    } 
+  }
+}
+
+function grid(cols, rows) {
+  let emptyGrid = [];
+  for (let i = 0; i < cols; i++) {
+    emptyGrid.push([])
+    for (let j = 0; j < rows; j++) {
+      emptyGrid[i].push(0);
+    }
+  }
+  return emptyGrid;
+}
+
+function displayGrid(grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 0) {
+        rect(i * CELLSIZE, j * CELLSIZE, CELLSIZE, CELLSIZE);
+      }
+    }
+  }
 }
